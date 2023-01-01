c67a4490a61574bb3a81ffc2fd8c422cf9a391f7
Taegu <gmdals45@gmail.com>
2023-01-01 17:20:39 +0900

    [2022/01/01 typescript script 추가]

--- scripts/deploy_1.sh
+++ scripts/deploy_1.sh
@@ -46,6 +46,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
+        -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.properties \
         $REPOSITORY/$JAR_NAME 2>&1 &
 
